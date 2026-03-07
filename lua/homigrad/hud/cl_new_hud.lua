645dbfffccbb774e486954ef07dd5a76c8c2cbd3
mrboilol <universodeiker@gmail.com>
2026-03-06 21:31:07 -0700

    bmore

--- lua/homigrad/hud/cl_new_hud.lua
+++ lua/homigrad/hud/cl_new_hud.lua
@@ -15,18 +15,18 @@ local function draw_hud()
 end
 hook.Add("HUDPaint", "homigrad_new_hud", draw_hud)
 
-local function toggle_hud()
+local function toggle_hud(ply, bind, pressed)
     if not hud_enabled:GetBool() then return end
-    
-    -- Get the key from the convar
-    local key_name = hud_open_key:GetString()
-    local key = input.LookupBinding(key_name)
-    
-    -- If the key is not bound, do nothing
-    if key == 0 then return end
 
-    if input.IsKeyReleased(key) then
+    local key_name = hud_open_key:GetString()
+    if string.lower(bind) == "gm_showhelp" and key_name == "f1" then
         hud_open = not hud_open
+        return true
+    end
+
+    if string.lower(bind) == key_name then
+        hud_open = not hud_open
+        return true
     end
 end
-hook.Add("Think", "homigrad_new_hud_toggle", toggle_hud)
+hook.Add("PlayerBindPress", "homigrad_new_hud_toggle", toggle_hud)
